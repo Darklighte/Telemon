5c77b05066d705a773f9c9ea36d674a7e45f5abf
Aranjah <anna.l.whitley@gmail.com>
2013-08-23 14:12:55 -0400

    fixing brackets and adding an empty listener
    See title.

--- Eclipse/CommListener/src/com/rucks/testlib/TestLibMain.java
+++ Eclipse/CommListener/src/com/rucks/testlib/TestLibMain.java
@@ -5,20 +5,20 @@ import android.view.Menu;
 
 import com.unity3d.player.UnityPlayerActivity;
 
-public class TestLibMain extends UnityPlayerActivity {
-
+public class TestLibMain extends UnityPlayerActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_test_lib_main);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.test_lib_main, menu);
         return true;
     }
-    
 }
