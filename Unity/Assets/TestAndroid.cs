5c873af6ff7b02629596dccd2155087bfb106d08
Darklighte <kevinrucks@hotmail.com>
2013-08-27 16:13:51 -0400

    dllnotfound rawr

--- Unity/Assets/TestAndroid.cs
+++ Unity/Assets/TestAndroid.cs
@@ -6,12 +6,16 @@ using System;
 public class TestAndroid : MonoBehaviour {
 	
 	[DllImport("commsetup")]
-	private static extern int fibonacci();
+	private static extern void printLog();
 	
+	//public string touch;
 	//AndroidJavaClass unityPlayer = new AndroidJavaClass("com.rucks.testlib.TestLibMain");
 	//AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
 	//AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
+	
+	
+	
+	
 	// Use this for initialization
 	void Start () {
 //		AndroidJNIHelper.debug = true; 
@@ -19,17 +23,36 @@ public class TestAndroid : MonoBehaviour {
 //			jc.CallStatic("UnitySendMessage", "Main Camera", "JavaMessage", "whoowhoo"); 
 //		} 
 		
+		if( System.IO.File.Exists("libcommsetup.se") )
+		{
+			Debug.Log("FILE EXIST");	
+		}
+		else
+		{
+			Debug.Log("NOOOOOOOOO");	
+		}
+
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//touch = Input.GetTouch(0);
+		
+		if(	Input.touchCount> 0 && 
+			Input.GetTouch(0).position.x > 20 && 
+			Input.GetTouch(0).position.x < 320 &&
+			Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			printLog();
+		}
 	}
 	
 	void OnGUI () {
-		if (GUI.Button (new Rect (10,10,150,100), "I am a button")) {
+		if (GUI.Button (new Rect (20,20,300,200), "I am a button")) {
 			print ("You clicked the button!");
-			fibonacci();
+			Debug.Log("stringPtr = " +printLog());
 		}
+		//GUI.TextArea(new Rect (20, 350, 300, 200), "TEXT");
 	}
 }
