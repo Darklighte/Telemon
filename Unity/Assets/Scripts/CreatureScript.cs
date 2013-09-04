900648a9830077ec29c2663ef4f13e35c207a1c9
Aranjah <anna.l.whitley@gmail.com>
2013-09-03 20:56:13 -0400

    minor positioning change

--- Unity/Assets/Scripts/CreatureScript.cs
+++ Unity/Assets/Scripts/CreatureScript.cs
@@ -20,10 +20,10 @@ public class CreatureScript : MonoBehaviour {
 		int widthDev2 = Screen.width/2;
 		int heightDev2 = Screen.height/2;
 		int left = Screen.width - (3*widthDev2/2);
-		int top = heightDev2/2;
+		int top = 3*heightDev2/2;
 		
 		long atk = 0;
 		long def = 0;
-		GUI.Box(new Rect(left,top,widthDev2, top), "\n\n\nStats\n\nAttack: " + atk + "\nDefense: " + def);
+		GUI.Box(new Rect(left,top,widthDev2, top/3), "\n\n\nStats\n\nAttack: " + atk + "\nDefense: " + def);
 	}
 }
