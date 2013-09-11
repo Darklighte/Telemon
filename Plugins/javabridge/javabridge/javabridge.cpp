74c3d58de22fbaeeb0d7a08973be912041420e05
Aranjah <anna.l.whitley@gmail.com>
2013-09-11 13:34:49 -0400

    nothing
    R.java is always  "to be committed" for me, probably because it's
    auto-generated. javabridge.cpp I've been messing with but it doesn't
    crash.

--- Plugins/javabridge/javabridge/javabridge.cpp
+++ Plugins/javabridge/javabridge/javabridge.cpp
@@ -1,14 +1,26 @@
 #include <stdlib.h>
-//#include <jni.h>
+#include <jni.h>
 
 
 extern "C"
 {
+	int blah = 0;
 
-	__declspec(dllexport) int hereIAm()
+	JNIEXPORT int hereIAm()
 	{
-		return 5;
+		return blah++;
 	}
+
+
+
+
+
+
+
+
+
+
+
 
 	//JavaVM*	java_vm;
 	//jobject mainActivityObj;
@@ -16,22 +28,22 @@ extern "C"
 
 	//JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved)
 	//{
-	//	// use __android_log_print for logcat debugging.
-	//	//__android_log_print(ANDROID_LOG_INFO, "JavaBridge", "[%s] Creating java link vm = %08x\n", __FUNCTION__, vm);
+	////	// use __android_log_print for logcat debugging.
+	////	//__android_log_print(ANDROID_LOG_INFO, "JavaBridge", "[%s] Creating java link vm = %08x\n", __FUNCTION__, vm);
 
-	//	//save off VM pointer
+	////	//save off VM pointer
 	//	java_vm = vm;
 
-	//	// attach our thread to the java vm; (it's already attached but this way we get the JNIEnv.)
+	////	// attach our thread to the java vm; (it's already attached but this way we get the JNIEnv.)
 	//	JNIEnv* jni_env = 0;
 	//	java_vm->AttachCurrentThread((void**)&jni_env, 0);
 	//	//__android_log_print(ANDROID_LOG_INFO, "JavaBridge", "[%s] JNI Environment is = %08x\n", __FUNCTION__, jni_env);
 
-	//	// try to find our main activity.
+	////	// try to find our main activity.
 	//	jclass cls_Activity		= jni_env->FindClass("com/unity3d/player/UnityPlayer");
 	//	jfieldID fid_Activity	= jni_env->GetStaticFieldID(cls_Activity, "currentActivity", "Landroid/app/Activity;");
 	//	mainActivityObj			= jni_env->GetStaticObjectField(cls_Activity, fid_Activity);
-	//	//__android_log_print(ANDROID_LOG_INFO, "JavaBridge", "[%s] Current activity = %08x\n", __FUNCTION__, obj_Activity);
+	////	//__android_log_print(ANDROID_LOG_INFO, "JavaBridge", "[%s] Current activity = %08x\n", __FUNCTION__, obj_Activity);
 
 	//	//getActivityCacheDir	= jni_env->GetMethodID(cls_JavaClass, "getActivityCacheDir", "()Ljava/lang/String;");
 	//	jclass mainActivityClass = jni_env->GetObjectClass(mainActivityObj);
@@ -44,11 +56,11 @@ extern "C"
 	//{
 	//	int newCount = 0;
 
-	//	//reacquire jni env.
+	////	//reacquire jni env.
 	//	JNIEnv* jni_env = 0;
 	//	java_vm->AttachCurrentThread((void**)&jni_env, 0);
 
-	//	//call function?!?!?!?!
+	////	//call function?!?!?!?!
 	//	jint jCount = (jint)jni_env->CallObjectMethod(mainActivityObj, getSMSCount);
 	//	if(jCount != 0)
 	//	{
