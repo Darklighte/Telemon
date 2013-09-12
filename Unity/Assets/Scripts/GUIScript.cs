f0707bb8f0519d43df1e047dd0693c0fa2d24325
Darklighte <kevinrucks@hotmail.com>
2013-09-12 12:53:49 -0400

    Updated master GUI Script
    By way of copy and paste, cause it worked

--- Unity/Assets/Scripts/GUIScript.cs
+++ Unity/Assets/Scripts/GUIScript.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class GUIScript : MonoBehaviour {
 	
 	private CreatureScript creature;
+	public bool serviceToggle;
+	private bool displayPrivacyPolicy;
 	
 	// Use this for initialization
 	void Start () 
 	{
 		//Get a reference to the creature and hold it so we only have to get it once
 		creature =  (CreatureScript)GameObject.Find("Creature").GetComponent("CreatureScript");
+		
+		// Assume the service is off when we start.
+		// This should be initialized to the previously-set value.
+		serviceToggle = false;
+		
+		// Don't display the privacy policy at start.
+		displayPrivacyPolicy = false;
 	}
 	
 	// Update is called once per frame
@@ -20,27 +29,82 @@ public class GUIScript : MonoBehaviour {
 	
 	void OnGUI()
 	{
-		int widthDev2 = Screen.width/2;
-		int heightDev2 = Screen.height/2;
-		int left = Screen.width - (3*widthDev2/2);
-		int top = 3*heightDev2/2;
+		int width = Screen.width;
+		int height = Screen.height / 8;
+		int widthDiv2 = Screen.width / 2;
+		int left = 0;
+		int top = 6 * Screen.height / 8;
+		
+		//  Button size parameters
+		int topBtn = 7 * Screen.height / 8;
+		int leftBtn = 0;
+		int widthBtn = Screen.width / 3;
+		int heightBtn = Screen.height / 8;
+		
+		// Privacy Policy window size parameters
+		int topPriv = 0;
+		int leftPriv = 0;
+		int widthPriv = Screen.width;
+		int heightPriv = 6 * Screen.height / 8;
 		
 		if(Application.platform == RuntimePlatform.WindowsEditor)
 		{
 			//attack box
-			if (GUI.Button(new Rect(10,50,widthDev2, 30), "Simulate incoming text"))
+			if (GUI.Button(new Rect(10,50,widthDiv2, 30), "Simulate incoming text"))
 			{
-				creature.Defense++;
+				if(serviceToggle)
+					creature.Defense++;
 			}
 			
 			//defense box
-			if(GUI.Button(new Rect(10,85,widthDev2, 30), "Simulate outcoming text"))
+			if(GUI.Button(new Rect(10,85,widthDiv2, 30), "Simulate outcoming text"))
 			{
-				creature.Attack++;
+				if(serviceToggle)
+					creature.Attack++;
 			}
 		}
 		
+		// Reset Button
+		if(GUI.Button(new Rect(leftBtn, topBtn, widthBtn, heightBtn), "Reset"))
+		{
+			// Reset the creature's attack and defense values.
+			creature.Attack = 0;
+			creature.Defense = 0;
+		}
 		
-		GUI.Box(new Rect(left,top,widthDev2, top/3), "\n\n\nStats\n\nAttack: " + creature.Attack + "\nDefense: " + creature.Defense);
+		// Buttons for service toggle.
+		if(serviceToggle)
+		{
+			// Service On Button
+			if(GUI.Button(new Rect(leftBtn + widthBtn, topBtn, widthBtn, heightBtn), "Service is On"))
+			{
+				serviceToggle = !serviceToggle;
+			}
+		}
+		else
+		{
+			// Service Off Button
+			if(GUI.Button(new Rect(leftBtn + widthBtn, topBtn, widthBtn, heightBtn), "Service is Off"))
+			{
+				serviceToggle = !serviceToggle;
+			}
+		}
+		
+		// Privacy Policy
+		if(GUI.Button(new Rect(leftBtn + widthBtn*2, topBtn, widthBtn, heightBtn), "Privacy"))
+		{
+			displayPrivacyPolicy = !displayPrivacyPolicy;
+		}
+		
+		// Privacy Policy display box
+		if(displayPrivacyPolicy)
+		{
+			GUI.Box(new Rect(leftPriv, topPriv, widthPriv, heightPriv), 
+				"Privacy Policy:\n" +
+				"We're not dicks and not tracking your data personally.\n" +
+				"You should probably be more worried about Google doing that.");
+		}
+		
+		GUI.Box(new Rect(left, top, width, height), "\nStats\n\nAttack: " + creature.Attack + "\nDefense: " + creature.Defense);
 	}
 }
